67d38636105fd8499bcd407b1f1a2c3226f396c5
Tumuhimbise Godwin <65310616+ushergodwin@users.noreply.github.com>
2023-11-16 11:39:28 +0300

    dump

--- INSTALL/mogue_db.sql
+++ INSTALL/mogue_db.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 31, 2018 at 08:16 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.0.29
+-- Host: localhost
+-- Generation Time: Nov 16, 2023 at 08:37 AM
+-- Server version: 8.1.0
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `beds` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int UNSIGNED NOT NULL,
   `number` varchar(40) NOT NULL,
-  `room` int(10) UNSIGNED NOT NULL,
+  `room` int UNSIGNED NOT NULL,
   `status` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `beds`
@@ -41,7 +40,8 @@ CREATE TABLE `beds` (
 
 INSERT INTO `beds` (`id`, `number`, `room`, `status`) VALUES
 (1, '11', 1, 'empty'),
-(2, '12', 2, 'empty');
+(2, '12', 2, 'empty'),
+(3, '01', 3, 'empty');
 
 -- --------------------------------------------------------
 
@@ -50,12 +50,12 @@ INSERT INTO `beds` (`id`, `number`, `room`, `status`) VALUES
 --
 
 CREATE TABLE `bill` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `deceased` int(10) UNSIGNED NOT NULL,
-  `total` int(10) UNSIGNED DEFAULT NULL,
-  `paid_amount` int(11) NOT NULL,
-  `balance` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int UNSIGNED NOT NULL,
+  `deceased` int UNSIGNED NOT NULL,
+  `total` int UNSIGNED DEFAULT NULL,
+  `paid_amount` int NOT NULL,
+  `balance` int UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `bill`
@@ -71,24 +71,25 @@ INSERT INTO `bill` (`id`, `deceased`, `total`, `paid_amount`, `balance`) VALUES
 --
 
 CREATE TABLE `incoming_deceased` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int UNSIGNED NOT NULL,
   `fullname` varchar(150) NOT NULL,
   `gender` varchar(40) NOT NULL,
   `tag_number` varchar(40) NOT NULL,
   `serial_number` varchar(40) NOT NULL,
-  `relative_name` int(10) UNSIGNED DEFAULT NULL,
-  `relative_number` int(10) UNSIGNED DEFAULT NULL,
-  `room` int(10) UNSIGNED NOT NULL,
-  `bed` int(10) UNSIGNED NOT NULL,
+  `relative_name` int UNSIGNED DEFAULT NULL,
+  `relative_number` int UNSIGNED DEFAULT NULL,
+  `room` int UNSIGNED NOT NULL,
+  `bed` int UNSIGNED NOT NULL,
   `date` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `incoming_deceased`
 --
 
 INSERT INTO `incoming_deceased` (`id`, `fullname`, `gender`, `tag_number`, `serial_number`, `relative_name`, `relative_number`, `room`, `bed`, `date`) VALUES
-(1, 'elephant ndovu', 'male', '356789542', '1234689987543245', 1, 1, 1, 1, '2018-05-28');
+(1, 'elephant ndovu', 'male', '356789542', '1234689987543245', 1, 1, 1, 1, '2018-05-28'),
+(2, 'Mugisha Robert', 'male', 'MR2023', 'MR83738J3983', 2, 2, 3, 3, '2023-11-14');
 
 -- --------------------------------------------------------
 
@@ -97,22 +98,23 @@ INSERT INTO `incoming_deceased` (`id`, `fullname`, `gender`, `tag_number`, `seri
 --
 
 CREATE TABLE `invoices` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `deceased` int(10) UNSIGNED NOT NULL,
-  `room` int(10) UNSIGNED DEFAULT NULL,
-  `relative` int(10) UNSIGNED DEFAULT NULL,
+  `id` int UNSIGNED NOT NULL,
+  `deceased` int UNSIGNED NOT NULL,
+  `room` int UNSIGNED DEFAULT NULL,
+  `relative` int UNSIGNED DEFAULT NULL,
   `services` text NOT NULL,
-  `total` int(11) NOT NULL,
-  `balance` int(11) DEFAULT NULL,
+  `total` int NOT NULL,
+  `balance` int DEFAULT NULL,
   `date` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `invoices`
 --
 
 INSERT INTO `invoices` (`id`, `deceased`, `room`, `relative`, `services`, `total`, `balance`, `date`) VALUES
-(3, 1, 1, 1, 'washing\r\nmaintain\r\nclean\r\ninject', 20000, 15000, '2018-05-28');
+(3, 1, 1, 1, 'washing\r\nmaintain\r\nclean\r\ninject', 20000, 15000, '2018-05-28'),
+(4, 2, 2, 2, 'Body Cleansing', 20000, 20000, '2023-11-15');
 
 -- --------------------------------------------------------
 
@@ -121,27 +123,36 @@ INSERT INTO `invoices` (`id`, `deceased`, `room`, `relative`, `services`, `total
 --
 
 CREATE TABLE `membership_grouppermissions` (
-  `permissionID` int(10) UNSIGNED NOT NULL,
-  `groupID` int(11) DEFAULT NULL,
+  `permissionID` int UNSIGNED NOT NULL,
+  `groupID` int DEFAULT NULL,
   `tableName` varchar(100) DEFAULT NULL,
-  `allowInsert` tinyint(4) DEFAULT NULL,
-  `allowView` tinyint(4) NOT NULL DEFAULT '0',
-  `allowEdit` tinyint(4) NOT NULL DEFAULT '0',
-  `allowDelete` tinyint(4) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `allowInsert` tinyint DEFAULT NULL,
+  `allowView` tinyint NOT NULL DEFAULT '0',
+  `allowEdit` tinyint NOT NULL DEFAULT '0',
+  `allowDelete` tinyint NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `membership_grouppermissions`
 --
 
 INSERT INTO `membership_grouppermissions` (`permissionID`, `groupID`, `tableName`, `allowInsert`, `allowView`, `allowEdit`, `allowDelete`) VALUES
-(1, 2, 'incoming_deceased', 1, 3, 3, 3),
-(2, 2, 'outgoing_deceased', 1, 3, 3, 3),
-(3, 2, 'relatives_info', 1, 3, 3, 3),
-(4, 2, 'rooms', 1, 3, 3, 3),
-(5, 2, 'beds', 1, 3, 3, 3),
-(6, 2, 'bill', 1, 3, 3, 3),
-(7, 2, 'invoices', 1, 3, 3, 3);
+(8, 2, 'incoming_deceased', 1, 3, 3, 3),
+(9, 2, 'outgoing_deceased', 1, 3, 3, 3),
+(10, 2, 'relatives_info', 1, 3, 3, 3),
+(11, 2, 'rooms', 1, 3, 3, 3),
+(12, 2, 'beds', 1, 3, 3, 3),
+(13, 2, 'bill', 1, 3, 3, 3),
+(14, 2, 'invoices', 1, 3, 3, 3),
+(15, 2, 'services', 1, 3, 3, 3),
+(32, 3, 'incoming_deceased', 0, 1, 1, 0),
+(33, 3, 'outgoing_deceased', 0, 0, 0, 0),
+(34, 3, 'relatives_info', 1, 1, 1, 1),
+(35, 3, 'rooms', 0, 3, 0, 0),
+(36, 3, 'beds', 0, 3, 0, 0),
+(37, 3, 'bill', 0, 3, 0, 0),
+(38, 3, 'invoices', 0, 1, 0, 0),
+(39, 3, 'services', 0, 3, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -150,12 +161,12 @@ INSERT INTO `membership_grouppermissions` (`permissionID`, `groupID`, `tableName
 --
 
 CREATE TABLE `membership_groups` (
-  `groupID` int(10) UNSIGNED NOT NULL,
+  `groupID` int UNSIGNED NOT NULL,
   `name` varchar(20) DEFAULT NULL,
   `description` text,
-  `allowSignup` tinyint(4) DEFAULT NULL,
-  `needsApproval` tinyint(4) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `allowSignup` tinyint DEFAULT NULL,
+  `needsApproval` tinyint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `membership_groups`
@@ -163,7 +174,8 @@ CREATE TABLE `membership_groups` (
 
 INSERT INTO `membership_groups` (`groupID`, `name`, `description`, `allowSignup`, `needsApproval`) VALUES
 (1, 'anonymous', 'Anonymous group created automatically on 2018-05-28', 0, 0),
-(2, 'Admins', 'Admin group created automatically on 2018-05-28', 0, 1);
+(2, 'Admins', 'Admin group created automatically on 2018-05-28', 0, 1),
+(3, 'Relatives', 'View Manager for Ralatives can only view', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -172,14 +184,14 @@ INSERT INTO `membership_groups` (`groupID`, `name`, `description`, `allowSignup`
 --
 
 CREATE TABLE `membership_userpermissions` (
-  `permissionID` int(10) UNSIGNED NOT NULL,
+  `permissionID` int UNSIGNED NOT NULL,
   `memberID` varchar(20) NOT NULL,
   `tableName` varchar(100) DEFAULT NULL,
-  `allowInsert` tinyint(4) DEFAULT NULL,
-  `allowView` tinyint(4) NOT NULL DEFAULT '0',
-  `allowEdit` tinyint(4) NOT NULL DEFAULT '0',
-  `allowDelete` tinyint(4) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `allowInsert` tinyint DEFAULT NULL,
+  `allowView` tinyint NOT NULL DEFAULT '0',
+  `allowEdit` tinyint NOT NULL DEFAULT '0',
+  `allowDelete` tinyint NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -188,14 +200,14 @@ CREATE TABLE `membership_userpermissions` (
 --
 
 CREATE TABLE `membership_userrecords` (
-  `recID` bigint(20) UNSIGNED NOT NULL,
+  `recID` bigint UNSIGNED NOT NULL,
   `tableName` varchar(100) DEFAULT NULL,
   `pkValue` varchar(255) DEFAULT NULL,
   `memberID` varchar(20) DEFAULT NULL,
-  `dateAdded` bigint(20) UNSIGNED DEFAULT NULL,
-  `dateUpdated` bigint(20) UNSIGNED DEFAULT NULL,
-  `groupID` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `dateAdded` bigint UNSIGNED DEFAULT NULL,
+  `dateUpdated` bigint UNSIGNED DEFAULT NULL,
+  `groupID` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `membership_userrecords`
@@ -209,7 +221,14 @@ INSERT INTO `membership_userrecords` (`recID`, `tableName`, `pkValue`, `memberID
 (5, 'relatives_info', '1', 'admin', 1527492535, 1527492535, 2),
 (6, 'incoming_deceased', '1', 'admin', 1527493086, 1527493086, 2),
 (9, 'invoices', '3', 'admin', 1527493680, 1527493680, 2),
-(10, 'bill', '1', 'admin', 1527494036, 1527494036, 2);
+(10, 'bill', '1', 'admin', 1527494036, 1527494036, 2),
+(11, 'relatives_info', '2', 'admin', 1700023280, 1700023291, 2),
+(12, 'rooms', '3', 'admin', 1700023323, 1700023323, 2),
+(13, 'beds', '3', 'admin', 1700023349, 1700023349, 2),
+(14, 'incoming_deceased', '2', 'david', 1700023358, 1700061286, 3),
+(15, 'invoices', '4', 'david', 1700024685, 1700061536, 3),
+(16, 'services', '1', 'admin', 1700032694, 1700032694, 2),
+(17, 'services', '2', 'admin', 1700057628, 1700057628, 2);
 
 -- --------------------------------------------------------
 
@@ -222,24 +241,25 @@ CREATE TABLE `membership_users` (
   `passMD5` varchar(40) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `signupDate` date DEFAULT NULL,
-  `groupID` int(10) UNSIGNED DEFAULT NULL,
-  `isBanned` tinyint(4) DEFAULT NULL,
-  `isApproved` tinyint(4) DEFAULT NULL,
+  `groupID` int UNSIGNED DEFAULT NULL,
+  `isBanned` tinyint DEFAULT NULL,
+  `isApproved` tinyint DEFAULT NULL,
   `custom1` text,
   `custom2` text,
   `custom3` text,
   `custom4` text,
   `comments` text,
   `pass_reset_key` varchar(100) DEFAULT NULL,
-  `pass_reset_expiry` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `pass_reset_expiry` int UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `membership_users`
 --
 
 INSERT INTO `membership_users` (`memberID`, `passMD5`, `email`, `signupDate`, `groupID`, `isBanned`, `isApproved`, `custom1`, `custom2`, `custom3`, `custom4`, `comments`, `pass_reset_key`, `pass_reset_expiry`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@admin.com', '2018-05-28', 2, 0, 1, NULL, NULL, NULL, NULL, 'Admin member created automatically on 2018-05-28', NULL, NULL),
+('admin', '8d804a5c53b69a7342c5c3c7ddc5364d', 'admin@admin.com', '2018-05-28', 2, 0, 1, NULL, NULL, NULL, NULL, 'Admin member created automatically on 2018-05-28\nRecord updated automatically on 2023-11-15', NULL, NULL),
+('david', 'f4b1b630b5c4d45ea9843c9fb32b056f', 'david@klacitymortuary.com', '2023-11-15', 3, 0, 1, 'Kajimu David', 'Makerere University', 'Kampala', 'Kampala Region', 'member signed up through the registration form.', NULL, NULL),
 ('guest', NULL, NULL, '2018-05-28', 1, 0, 1, NULL, NULL, NULL, NULL, 'Anonymous member created automatically on 2018-05-28', NULL, NULL);
 
 -- --------------------------------------------------------
@@ -249,16 +269,16 @@ INSERT INTO `membership_users` (`memberID`, `passMD5`, `email`, `signupDate`, `g
 --
 
 CREATE TABLE `outgoing_deceased` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `fullname` int(10) UNSIGNED NOT NULL,
-  `gender` int(10) UNSIGNED DEFAULT NULL,
-  `tag_number` int(10) UNSIGNED DEFAULT NULL,
-  `serial_number` int(10) UNSIGNED DEFAULT NULL,
-  `relative_name` int(10) UNSIGNED DEFAULT NULL,
-  `relative_phone_number` int(10) UNSIGNED DEFAULT NULL,
+  `id` int UNSIGNED NOT NULL,
+  `fullname` int UNSIGNED NOT NULL,
+  `gender` int UNSIGNED DEFAULT NULL,
+  `tag_number` int UNSIGNED DEFAULT NULL,
+  `serial_number` int UNSIGNED DEFAULT NULL,
+  `relative_name` int UNSIGNED DEFAULT NULL,
+  `relative_phone_number` int UNSIGNED DEFAULT NULL,
   `date` date DEFAULT NULL,
   `car_out_number` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -267,7 +287,7 @@ CREATE TABLE `outgoing_deceased` (
 --
 
 CREATE TABLE `relatives_info` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int UNSIGNED NOT NULL,
   `first_relative_full_name` varchar(150) NOT NULL,
   `home_address` varchar(200) NOT NULL,
   `home_town` varchar(200) NOT NULL,
@@ -278,14 +298,15 @@ CREATE TABLE `relatives_info` (
   `second_relative_home_town` varchar(40) DEFAULT NULL,
   `second_relative_occupation` varchar(40) DEFAULT NULL,
   `second_relative_phone_number` varchar(40) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `relatives_info`
 --
 
 INSERT INTO `relatives_info` (`id`, `first_relative_full_name`, `home_address`, `home_town`, `occupation`, `phone_number`, `second_relative_full_name`, `second_relative_home_address`, `second_relative_home_town`, `second_relative_occupation`, `second_relative_phone_number`) VALUES
-(1, 'lion simba', 'junge', 'den', 'king', '09876543', NULL, NULL, NULL, NULL, NULL);
+(1, 'lion simba', 'junge', 'den', 'king', '09876543', NULL, NULL, NULL, NULL, NULL),
+(2, 'Tumuhimbise Godwin', 'Kampala', 'Kampala', 'Software Engineer', '+256741033402', NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -294,11 +315,11 @@ INSERT INTO `relatives_info` (`id`, `first_relative_full_name`, `home_address`, 
 --
 
 CREATE TABLE `rooms` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int UNSIGNED NOT NULL,
   `name` varchar(40) NOT NULL,
   `type` varchar(40) NOT NULL,
   `status` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `rooms`
@@ -306,7 +327,29 @@ CREATE TABLE `rooms` (
 
 INSERT INTO `rooms` (`id`, `name`, `type`, `status`) VALUES
 (1, 'A20', 'VIP', 'available'),
-(2, 'A22', 'Ordinary', 'available');
+(2, 'A22', 'Ordinary', 'available'),
+(3, 'A23', 'VIP', 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int NOT NULL,
+  `name` varchar(199) NOT NULL,
+  `cost` int NOT NULL,
+  `rate` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'None'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `services`
+--
+
+INSERT INTO `services` (`id`, `name`, `cost`, `rate`) VALUES
+(1, 'Embalming', 200000, 'None'),
+(2, 'Room Service', 50000, 'Per Hour');
 
 --
 -- Indexes for dumped tables
@@ -398,6 +441,12 @@ ALTER TABLE `rooms`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -405,67 +454,73 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `beds`
 --
 ALTER TABLE `beds`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `incoming_deceased`
 --
 ALTER TABLE `incoming_deceased`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `membership_grouppermissions`
 --
 ALTER TABLE `membership_grouppermissions`
-  MODIFY `permissionID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `permissionID` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `membership_groups`
 --
 ALTER TABLE `membership_groups`
-  MODIFY `groupID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `groupID` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `membership_userpermissions`
 --
 ALTER TABLE `membership_userpermissions`
-  MODIFY `permissionID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `permissionID` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `membership_userrecords`
 --
 ALTER TABLE `membership_userrecords`
-  MODIFY `recID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `recID` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `outgoing_deceased`
 --
 ALTER TABLE `outgoing_deceased`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `relatives_info`
 --
 ALTER TABLE `relatives_info`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
